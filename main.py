186873f62e2da5fa377bd11fa17404d0b3e0fa65
João Vitor <joaovitorgmartins22@gmail.com>
2023-10-10 14:05:37 -0300

    mudancas maulcas do caio, comecamos galera

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
 
 app = Flask (__name__)
 app.secret_key = "trabalhinho"
@@ -6,4 +8,13 @@ app.secret_key = "trabalhinho"
 @app.route("/")
 
 def hello():
-    return render_template("login.html")
+    return render_template("home.html")
+
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+   return render_template("home.html")
+
+app.run()
