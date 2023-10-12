ce5984d40862ad42a5bd39c4f456a7d9ec9f5622
costa-dani <danicl.souza2004@gmail.com>
2023-10-12 00:33:14 -0300

    mais do front
    - html e css da pagina de reclamação
    - começo da home (barra das três opções)
    - html e inicio do css da pagina de "reclamação enviada com sucesso"

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 
-
 app = Flask (__name__)
 app.secret_key = "trabalhinho"
 
@@ -19,8 +18,7 @@ def hello():
        
       elif escolha == "login":
 
-         #checar se o nome e senha estao no banco de dados
-         return 1
+         return render_template("home.html")
 
    return render_template("login.html")
 
@@ -30,17 +28,75 @@ def cadastro():
 
    if request.method == 'POST':
 
-      escolha = request.form.get("botao")
+      escolha2 = request.form.get("botao")
 
-      if escolha == "cadastrar":
+      if escolha2 == "cadastrar":
 
          #colocar o nome e senha no banco de dados
-         return 1
+         return render_template("home.html")
       
-      elif escolha == "irlogin":
+      elif escolha2 == "irlogin":
 
          return render_template("login.html")
 
    return render_template("cadastro.html")
+
+@app.route("/home", methods = ["POST", "GET"])
+
+def home():
+
+   if request.method == 'POST':
+
+      escolha3 = request.form.get("botao")
+
+      if escolha3 == "home":
+
+         return render_template("home.html")
+       
+      elif escolha3 == "reclamar":
+
+         return render_template("reclamacao.html")
+      
+      elif escolha3 == "resolvido":
+
+         return render_template("resolvidas.html")
+
+   return render_template("home.html")
+
+@app.route("/reclamacao", methods = ["POST", "GET"])
+
+def reclamacao():
+
+   if request.method == 'POST':
+      
+      escolha4 = request.form.get("botao")
+
+      if escolha4 == "reclamou":
+
+         return render_template("sucesso.html")
+      
+      elif escolha4 == "home":
+
+         return render_template("home.html")
+
+   return render_template("reclamacao.html")
+
+@app.route("/sucesso", methods = ["POST", "GET"])
+
+def sucesso():
+
+   if request.method == 'POST':
+      
+      escolha5 = request.form.get("botao")
+
+      if escolha5 == "outrarec":
+
+         return render_template("reclamacao.html")
+      
+      elif escolha5 == "voltarhome":
+
+         return render_template("home.html")
+
+   return render_template("sucesso.html")
 
 app.run()
