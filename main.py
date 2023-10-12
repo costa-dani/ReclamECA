4db46312ab3c544bfc19335e7cb6811f99779deb
costa-dani <danicl.souza2004@gmail.com>
2023-10-11 21:35:51 -0300

    começo do front
    - html do login e do cadastro
    - basico do css no login e no cadastro (sem paleta de cores definida ainda)
    - relacionei login e cadastro na main

--- main.py
+++ main.py
@@ -5,16 +5,42 @@ from werkzeug.utils import secure_filename
 app = Flask (__name__)
 app.secret_key = "trabalhinho"
 
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 
 def hello():
-    return render_template("home.html")
-
-def upload_file():
+   
    if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
-   return render_template("home.html")
+
+      escolha = request.form.get("botao")
+
+      if escolha == "signup":
+
+         return render_template("cadastro.html")
+       
+      elif escolha == "login":
+
+         #checar se o nome e senha estao no banco de dados
+         return 1
+
+   return render_template("login.html")
+
+@app.route("/signup", methods = ["POST", "GET"])
+
+def cadastro():
+
+   if request.method == 'POST':
+
+      escolha = request.form.get("botao")
+
+      if escolha == "cadastrar":
+
+         #colocar o nome e senha no banco de dados
+         return 1
+      
+      elif escolha == "irlogin":
+
+         return render_template("login.html")
+
+   return render_template("cadastro.html")
 
 app.run()
